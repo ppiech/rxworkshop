154c6138b8c54b997306d3b980d6abdf159d1a85
Pawel Piech <pawel.piech@lookout.com>
2016-11-11 09:04:44 -0800

    Add exercises for Observable.create()

--- lib/src/main/java/rxworkshop/create/Example.java
+++ lib/src/main/java/rxworkshop/create/Example.java
@@ -3,6 +3,15 @@ package rxworkshop.create;
 import rx.Observable;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+// Exercise 1: Use Obsevable.create() to make an obsevable that returns  current time (System.currentTimeMillis()).
+// Exercise 2: Implement observable that keeps emitting time using the Timer class below.
+// Exercise 3: Use exmaple with observeOn() and a short interval to trigger a MissingBackpressureException
+// Exercise 4: Convert 1, to use fromCallable().
+// Exercise 5: Convert 2, to use fromEmittable().
+// Exercise 6: Repteat 3 with fromEmittable();
 
 public class Example {
 
@@ -10,10 +19,42 @@ public class Example {
         series().subscribe(value -> System.out.print(", " + value));
     }
 
-
     static Observable<Integer> series() {
         return Observable.from(Arrays.asList(1, 2, 3));
     }
+}
 
+interface TimerListner {
+    public void onNewTime(long time);
+}
+
+class Timer {
+    final long interval;
+    AtomicReference<TimerListner> listenerRef;
+
+    Timer(long interval) {
+        this.interval = interval;
+        new Thread(() -> {
+            while (true) {
+                TimerListner listener = listenerRef.get();
+                if (listener != null) {
+                    listener.onNewTime(System.currentTimeMillis());
+                }
+                try {
+                    Thread.sleep(interval);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        });
+    }
+
+    void setListener(TimerListner listener) {
+        listenerRef.set(listener);
+    }
+
+    void clearListener() {
+        listenerRef.set(null);
+    }
 
 }
